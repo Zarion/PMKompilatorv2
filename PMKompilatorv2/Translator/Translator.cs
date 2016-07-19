8e443d2adc96aab6233ac2ff963d83de627ea900
Daniel Rynca <daniel.rynca@interia.pl>
2016-07-19 09:51:44 +0200

    Drobne zmiany

---
+++ PMKompilatorv2/Translator/Translator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMKompilatorv2.Translator
+{
+    public class Translator
+    {
+        private Translator()
+        {
+            //not implemented yet
+        }
+    }
+}
